221160d2ddbeda93404a75b0ddce68fa09f5047a
Santiago Castro <santywin@gmail.com>
2020-02-29 12:11:18 -0500

    Se agrega la data y se empieza el archivo con la importancion de las librerias

--- Scripts/Primero.R
+++ Scripts/Primero.R
@@ -1,1 +1,20 @@
-hola
+library(dplyr)
+library(tidydr)
+library(ggplot2)
+library(magrittr)
+library(cluster)
+library(readr)
+library(readxl)
+
+
+df_wb <-read_delim("data/base_wb.csv", delim = ";")
+
+
+
+
+
+
+
+
+
+
